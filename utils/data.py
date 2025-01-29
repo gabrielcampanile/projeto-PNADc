694369fe9fe7bfda6051aeb187b553a191a74a16
Gabriel Belchior Vieira Campanile <campanile@unifesp.br>
2025-01-29 18:05:30 -0300

    Remove unused seaborn import from data utility module

--- utils/data.py
+++ utils/data.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
-import seaborn as sns
 import geopandas as gpd
 from scipy.stats import linregress
 
